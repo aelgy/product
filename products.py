cef9b79cf59e76fda65624008271a39465c952c5
Duke Chen <aelgy333@gmail.com>
2023-07-04 15:05:18 +0800

    add header]

--- products.py
+++ products.py
@@ -4,12 +4,15 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
-	# p = []
-	# p.append(name) # 把 name 放入 p
-	# p.append(price) # 把 price 也同時放入 p
-	# p = [name, price]
+	price = int(price)
 	products.append([name, price]) # 把 p 放入 products 清單中
 print(products)
 
 for p in products:
-	print(p)
+	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品, 價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
+
