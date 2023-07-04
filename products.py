9aa90a74d28bec8a5cc4422c7c4355e2cb0af95a
Duke Chen <aelgy333@gmail.com>
2023-07-04 11:52:16 +0800

    add new file

---
+++ products.py
@@ -0,0 +1,12 @@
+products = []
+while True:
+	name = input('請輸入商品名稱：')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格:')
+	# p = []
+	# p.append(name) # 把 name 放入 p
+	# p.append(price) # 把 price 也同時放入 p
+	# p = [name, price]
+	products.append([name, price]) # 把 p 放入 products 清單中
+print(products)
